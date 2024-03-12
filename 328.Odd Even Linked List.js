6214033f2c63395b99f4f8cb835581e4206afd63
anna <dodonova.anna76@gmail.com>
2024-03-12 01:31:30 +0100

    linked list

--- 328.Odd Even Linked List.js
+++ 328.Odd Even Linked List.js
@@ -10,5 +10,23 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
+    if (!head?.next) {
+        return head
+    }
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
 
+    while(even.next) {
+        odd.next = even.next
+        odd = odd.next
+        if (odd.next) {
+            even.next = odd.next;
+            even = even.next;
+        } else {
+            even.next = null
+        }
+    }
+    odd.next = evenHead;
+    return head;
 };
