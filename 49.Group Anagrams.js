3fa2f94de1074c21aed0c55ca43eace40d952b90
annadodonova <annadodonova@microsoft.com>
2024-03-19 12:50:41 +0100

    apple

---
+++ 49.Group Anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const dict = {};
+    strs.forEach((word) => {
+        const sortedWord = word.split('').sort().join('');
+        if (dict[sortedWord] === undefined) {
+            dict[sortedWord] = [];
+        }
+        dict[sortedWord].push(word);
+    })
+    const ans = [];
+    for (key in dict) {
+        ans.push(dict[key]);
+    }
+    return ans;
+};
