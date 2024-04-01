b61c31b808b882c71ac7d49a667b4ddb2a7c86b6
akellaX <dodonova.anna76@gmail.com>
2024-04-02 01:24:20 +0200

    is valid bst

---
+++ 98. Validate Binary Search Tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    const isValid = (node, low, high) => {
+        if (!node) {
+            return true
+        }
+        if ((low !== undefined && low >= node.val) || (high != undefined && high <= node.val)) {
+            return false;
+        }
+
+        return isValid(node.right, node.val, high) && isValid(node.left, low, node.val);
+    }
+
+    return isValid(root)
+};
