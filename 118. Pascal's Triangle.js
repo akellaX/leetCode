10d3ad02a253ccefa106e7bdda8023395ef08712
anna <dodonova.anna76@gmail.com>
2024-04-09 21:53:30 +0200

    task 118

---
+++ 118. Pascal's Triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let rows = 1;
+    const triangle = [[1]];
+    while (numRows > rows) {
+        rows++;
+        const newRow = new Array(rows).fill(0);
+        newRow.forEach((el, i) => {
+            if (i === 0 || i === newRow.length - 1) {
+                newRow[i] = 1;
+            } else {
+                const lastRow = triangle[triangle.length-1];
+                newRow[i] = lastRow[i - 1] + lastRow[i];
+            }
+        })
+        triangle.push(newRow);
+    }
+    return triangle;
+};
